4144e13b99fa613a3109789c359a8aba69354403
sarafdez <sarafrcabello@gmail.com.com>
2017-04-10 11:02:39 +0200

    adding lm

---
+++ demo.R
@@ -0,0 +1,11 @@
+
+# From https://github.com/poldrack/reproducible-workflows
+
+# R code
+df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
+              header=TRUE,sep=',')
+head(df)
+
+# R code
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
