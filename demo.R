ee5014b91d60e6a52617c64526c7bed91e414a19
sarafdez <sarafrcabello@gmail.com.com>
2017-04-10 11:22:35 +0200

    adding outlier removal

--- demo.R
+++ demo.R
@@ -7,15 +7,19 @@ df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/Lskyeta
 head(df)
 
 # R code
+max_age=120
+min_age=18
+
+# R code
+df=subset(df,age>min_age&age<max_age)
+
+stopifnot(max(df$age)<max_age)
+stopifnot(min(df$age)>min_age)
+
+# R code
 lm.result=lm(conspiracist_avg~age,data=df)
 summary(lm.result)
 
 # R code
 plot(df$age,df$conspiracist_avg)
 
-# R code
-max_age=120
-min_age=18
-
-stopifnot(max(df$age)<max_age)
-stopifnot(min(df$age)>min_age)
